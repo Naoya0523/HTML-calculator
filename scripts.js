bafcc734215c4886cd1220356c3de07a439539d6
Naoya0523 <nintendonaoya@gmail.com>
2023-03-30 23:51:57 +0900

    day2

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
+let answer = ""
+
 let btn1 = document.getElementById('1');
 btn1.onclick = function(){
-    console.log('btn1 clicked');
+    answer += '1';
+    document.getElementById('ans').innerHTML = answer;
+}
+
+let btn_equal = document.getElementById('equal');
+btn_equal.onclick = function() {
+    ans = eval(answer);
+    document.getElementById('ans').innerHTML = ans.toString();
+}
+
+let btn_C = document.getElementById('C');
+btn_C.onclick = function() {
+    answer = ""
+    document.getElementById('ans').innerHTML = answer
 }
